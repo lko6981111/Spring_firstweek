fa95208c43cf44b9177e0b4a3425a11b11103ca6
lko6981111 <lko6981111@gmail.com>
2023-09-25 19:17:49 +0900

    1-1 스프링부트 시작하기

---
+++ src/main/java/com/example/firstproject/FirstprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //메인 메소드
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+}
